efbc59c2d0367153b1df752847b7df5ac813aa3c
Yaman.RedTeam <yamanblackhat7@gmail.com>
2025-12-31 14:01:36 +0530

    first commit

---
+++ index.sh
@@ -0,0 +1,17 @@
+#COMMANDS FOR INFORMATION GATHERING.
+
+whatis host  
+#DNS lookup
+
+whatweb  
+#web technology used   
+
+host 
+#ip address, etc
+
+extensions(wappalyzer, buildwith)
+#Techology used in details 
+ 
+webhttrack 
+#website copier download
+
